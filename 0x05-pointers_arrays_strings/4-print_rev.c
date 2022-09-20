e3486fdd4e58031d3aa4c3bc4c5d73f6f6e1c090
tomi <tomilijoka586@gmail.com>
2022-09-20 15:25:52 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,6 @@
  * print_rev - reverses the string 
  * @s - the string thats needs to be reversed
  * Return - nothing
- *
 */
 
 void print_rev(char *s){
@@ -12,9 +11,9 @@ int i = 0;
 while(s[i]){
 	i++;
 }
-while(i<=0){
+while(i--){
 	_putchar(s[i]);
-	i--;
+	
 }
 _putchar('\n');
 }
