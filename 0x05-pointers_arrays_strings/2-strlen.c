181ff79f4782a41190f7adce9317c46684557c87
tomi <tomilijoka586@gmail.com>
2022-09-20 14:25:18 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** 
+* strlen  - finds the length of the word
+*@s - the character passed into the function to check the length
+*Return - length of the word
+*
+*/
+
+int _strlen(char *s){
+int length = 0;
+
+while(*s++){
+length++
+}
+return (length);
+}
