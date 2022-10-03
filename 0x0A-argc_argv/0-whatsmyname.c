075ffb7e7624463c23867f1e91c132f7faa9d0cd
tomi <tomilijoka586@gmail.com>
2022-10-03 15:36:01 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc : number count of argumnets in the argv
+ * @argv : carries the list of passed arguments
+ * Return : Nothing
+ */
+
+void main(int argc char *argv[])
+{	(void)argc;
+	printf("%s\n",argv[0]);
+	return (0);
+
+}
+
+ 
